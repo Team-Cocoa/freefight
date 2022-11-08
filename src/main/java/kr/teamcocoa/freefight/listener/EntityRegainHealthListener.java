694bd676e9b61a752155c4759033031494f20720
fixca <jaehoon1744@gmail.com>
2022-11-08 17:50:13 +0900

    Diamond Pot 에서 자동 회복 활성화

--- src/main/java/kr/teamcocoa/freefight/listener/EntityRegainHealthListener.java
+++ src/main/java/kr/teamcocoa/freefight/listener/EntityRegainHealthListener.java
@@ -1,5 +1,10 @@
 package kr.teamcocoa.freefight.listener;
 
+import kr.teamcocoa.freefight.kits.Kits;
+import kr.teamcocoa.freefight.player.FreeFightPlayer;
+import kr.teamcocoa.freefight.player.FreeFightPlayerManager;
+import kr.teamcocoa.freefight.session.FreeFightSession;
+import kr.teamcocoa.freefight.session.SessionManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,12 +14,15 @@ public class EntityRegainHealthListener implements Listener {
 
     @EventHandler
     public void onHeal(EntityRegainHealthEvent e) {
-        if(!(e.getEntity() instanceof Player)) {
-            return;
-        }
-
-        if(e.getRegainReason() == EntityRegainHealthEvent.RegainReason.SATIATED) {
-            e.setCancelled(true);
+        if(e.getEntity() instanceof Player player) {
+            FreeFightPlayer freeFightPlayer = FreeFightPlayerManager.getPlayer(player);
+            FreeFightSession freeFightSession = SessionManager.getSession(freeFightPlayer);
+            if(e.getRegainReason() == EntityRegainHealthEvent.RegainReason.SATIATED) {
+                if(freeFightSession != null && freeFightSession.getKits() == Kits.DIAMOND_POT) {
+                    return;
+                }
+                e.setCancelled(true);
+            }
         }
     }
 
