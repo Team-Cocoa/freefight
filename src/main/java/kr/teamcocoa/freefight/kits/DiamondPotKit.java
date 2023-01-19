e5e03b59941067789688264cef36220086abc21a
fixca <jaehoon1744@gmail.com>
2023-01-19 10:08:26 +0900

    킷 저장 반영

--- src/main/java/kr/teamcocoa/freefight/kits/DiamondPotKit.java
+++ src/main/java/kr/teamcocoa/freefight/kits/DiamondPotKit.java
@@ -3,6 +3,7 @@ package kr.teamcocoa.freefight.kits;
 import kr.teamcocoa.freefight.main.FreeFight;
 import kr.teamcocoa.freefight.utils.ItemUtils;
 import kr.teamcocoa.freefight.utils.Pair;
+import kr.teamcocoa.kitmanager.frontend.main.KitManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -33,81 +34,9 @@ public class DiamondPotKit extends AbstractKit {
 
     @Override
     public void givePlayerKit(Player player) {
-        /*
-        * 갑옷 부분 시작
-        * */
-        List<Pair<Enchantment, Integer>> armorEnchantList = new ArrayList<>(3);
-        armorEnchantList.add(new Pair<>(Enchantment.MENDING, 1));
-        armorEnchantList.add(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 4));
-        armorEnchantList.add(new Pair<>(Enchantment.DURABILITY, 3));
-
-        ItemStack[] armorContent = new ItemStack[4];
-        armorContent[3] = new ItemStack(Material.DIAMOND_HELMET);
-        armorContent[2] = new ItemStack(Material.DIAMOND_CHESTPLATE);
-        armorContent[1] = new ItemStack(Material.DIAMOND_LEGGINGS);
-        armorContent[0] = new ItemStack(Material.DIAMOND_BOOTS);
-        for (ItemStack itemStack : armorContent) {
-            ItemUtils.enchant(itemStack, armorEnchantList);
-        }
-
-        /*
-         * 인벤 부분 시작
-         */
-        ItemStack[] inventory = new ItemStack[36];
-
-        List<Pair<Enchantment, Integer>> swordEnchantList = new ArrayList<>();
-        swordEnchantList.add(new Pair<>(Enchantment.DAMAGE_ALL, 5));
-        swordEnchantList.add(new Pair<>(Enchantment.DURABILITY, 3));
-        ItemStack sword = ItemUtils.enchant(new ItemStack(Material.DIAMOND_SWORD), swordEnchantList);
-
-        ItemStack pearl = new ItemStack(Material.ENDER_PEARL, 16);
-
-        ItemStack regen = new ItemStack(Material.SPLASH_POTION);
-        PotionMeta regenPotionMeta = (PotionMeta) regen.getItemMeta();
-        regenPotionMeta.setBasePotionData(new PotionData(PotionType.REGEN, true, false));
-        regen.setItemMeta(regenPotionMeta);
-
-        ItemStack speed = new ItemStack(Material.SPLASH_POTION);
-        PotionMeta speedPotionMeta = (PotionMeta) speed.getItemMeta();
-        speedPotionMeta.setBasePotionData(new PotionData(PotionType.SPEED, false, true));
-        speed.setItemMeta(speedPotionMeta);
-
-        ItemStack strength = new ItemStack(Material.SPLASH_POTION);
-        PotionMeta strengthPotionMeta = (PotionMeta) speed.getItemMeta();
-        strengthPotionMeta.setBasePotionData(new PotionData(PotionType.STRENGTH, false, true));
-        strength.setItemMeta(strengthPotionMeta);
-
-        ItemStack heal = new ItemStack(Material.SPLASH_POTION);
-        PotionMeta healPotionMeta = (PotionMeta) speed.getItemMeta();
-        healPotionMeta.setBasePotionData(new PotionData(PotionType.INSTANT_HEAL, false, true));
-        heal.setItemMeta(healPotionMeta);
-
-        inventory[0] = sword;
-        inventory[1] = pearl;
-
-        for(Integer i : Arrays.asList(6, 15, 24, 33)) {
-            inventory[i] = regen;
-        }
-
-        for (Integer i : Arrays.asList(7, 16, 25, 34)) {
-            inventory[i] = speed;
-        }
-
-        for (Integer i : Arrays.asList(8, 17, 26, 35)) {
-            inventory[i] = strength;
-        }
-
-        for(int i = 0; i < inventory.length; i++) {
-            if(inventory[i] == null) {
-                inventory[i] = heal;
-            }
-        }
-
         Bukkit.getScheduler().runTask(FreeFight.getInstance(), () -> {
            player.getInventory().clear();
-           player.getInventory().setContents(inventory);
-           player.getInventory().setArmorContents(armorContent);
-           player.getInventory().setItemInOffHand(new ItemStack(Material.GOLDEN_CARROT, 32));
+           KitManager.getKitManagerAPI().loadPlayerKit(player, kr.teamcocoa.kitmanager.frontend.kits.Kits.DIAMOND_POT);
         });
 
     }
--- src/main/java/kr/teamcocoa/freefight/kits/ShieldPvPKit.java
+++ src/main/java/kr/teamcocoa/freefight/kits/ShieldPvPKit.java
@@ -1,6 +1,7 @@
 package kr.teamcocoa.freefight.kits;
 
 import kr.teamcocoa.freefight.main.FreeFight;
+import kr.teamcocoa.kitmanager.frontend.main.KitManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -23,27 +24,9 @@ public class ShieldPvPKit extends AbstractKit {
 
     @Override
     public void givePlayerKit(Player player) {
-        ItemStack[] armorContent = new ItemStack[4];
-        armorContent[3] = new ItemStack(Material.DIAMOND_HELMET);
-        armorContent[2] = new ItemStack(Material.DIAMOND_CHESTPLATE);
-        armorContent[1] = new ItemStack(Material.DIAMOND_LEGGINGS);
-        armorContent[0] = new ItemStack(Material.DIAMOND_BOOTS);
-
-        ItemStack sword = new ItemStack(Material.DIAMOND_SWORD);
-        ItemStack axe = new ItemStack(Material.DIAMOND_AXE);
-        ItemStack crossbow = new ItemStack(Material.CROSSBOW);
-        ItemStack bow = new ItemStack(Material.BOW);
-        ItemStack arrow = new ItemStack(Material.ARROW, 7);
-        ItemStack shield = new ItemStack(Material.SHIELD);
         Bukkit.getScheduler().runTask(FreeFight.getInstance(), () -> {
             player.getInventory().clear();
-            player.getInventory().setArmorContents(armorContent);
-            player.getInventory().setItemInOffHand(shield);
-            player.getInventory().setItem(0, sword);
-            player.getInventory().setItem(1, axe);
-            player.getInventory().setItem(2, crossbow);
-            player.getInventory().setItem(3, bow);
-            player.getInventory().setItem(4, arrow);
+            KitManager.getKitManagerAPI().loadPlayerKit(player, kr.teamcocoa.kitmanager.frontend.kits.Kits.CLASSIC);
         });
     }
 }
