1d843f0c228a16058c52f891342c5f92d42ba04a
fixca <jaehoon1744@gmail.com>
2023-01-20 22:42:16 +0900

    메시지 모델 추가

---
+++ src/main/java/kr/teamcocoa/freefight/translation/messages/CantDuelMessage.java
@@ -0,0 +1,29 @@
+package kr.teamcocoa.freefight.translation.messages;
+
+import kr.teamcocoa.freefight.main.FreeFight;
+import kr.teamcocoa.freefight.translation.BaseMessage;
+import kr.teamcocoa.language.enums.TypeEnum;
+import kr.teamcocoa.language.languages.LanguageController;
+import org.bukkit.entity.Player;
+
+public class CantDuelMessage extends BaseMessage {
+
+    private static CantDuelMessage instance;
+
+    public static CantDuelMessage getInstance() {
+        if(instance == null) {
+            instance = new CantDuelMessage();
+        }
+        return instance;
+    }
+
+    private CantDuelMessage() {
+        super(Messages.CANT_DUEL_MESSAGE);
+    }
+
+    @Override
+    public String getMessage(Player player) {
+        String message = LanguageController.getMessage(player.getUniqueId(), TypeEnum.FREEFIGHT, getTranslatable().getNode());
+        return FreeFight.getPrefix() + message;
+    }
+}
