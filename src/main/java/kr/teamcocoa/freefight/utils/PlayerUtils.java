c8bf03ffa60ec573c2465d4aa3439a99bc7cf73d
fixca <jaehoon1744@gmail.com>
2023-03-04 17:04:42 +0900

    packet based bar 추가

--- src/main/java/kr/teamcocoa/freefight/utils/PlayerUtils.java
+++ src/main/java/kr/teamcocoa/freefight/utils/PlayerUtils.java
@@ -2,8 +2,11 @@ package kr.teamcocoa.freefight.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundChatPacket;
 import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitlesAnimationPacket;
@@ -21,6 +24,12 @@ public class PlayerUtils {
         ClientboundSetSubtitleTextPacket subTitlePacket = new ClientboundSetSubtitleTextPacket(Component.Serializer.fromJson("{\"text\": \"" + StringUtils.color(subtitle) + "\"}"));
 
         sendPackets(player, timePacket, titlePacket, subTitlePacket);
+    }
+
+    public static void sendBar(Player player, String message) {
+        MutableComponent msg = Component.Serializer.fromJson("{\"text\": \"" + message + "\"}");
+        ClientboundChatPacket packet = new ClientboundChatPacket(msg, ChatType.GAME_INFO, player.getUniqueId());
+        sendPackets(player, packet);
     }
 
     public static void sendPackets(Player player, Packet... packets) {
