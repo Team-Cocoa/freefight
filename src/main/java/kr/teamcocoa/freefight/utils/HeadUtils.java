f0a06854ffcbf5077665dbb449f9fd335f91698f
fixca <jaehoon1744@gmail.com>
2023-03-23 15:55:28 +0900

    플레이어 머리 유틸 추가

--- src/main/java/kr/teamcocoa/freefight/utils/HeadUtils.java
+++ src/main/java/kr/teamcocoa/freefight/utils/HeadUtils.java
@@ -4,6 +4,7 @@ package kr.teamcocoa.freefight.utils;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -16,23 +17,24 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HeadUtils {
 
-    private static HashMap<UUID, ItemStack> headCache = new HashMap<>();
+    @Getter
+    private static HashMap<UUID, String> headValueCache = new HashMap<>();
 
-//    public static ItemStack getHeadFromUUID(UUID uuid) {
-//        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
-//        SkullMeta meta = (SkullMeta) head.getItemMeta();
-//        GameProfile profile = new GameProfile(UUID.randomUUID(), "");
-//        profile.getProperties().put("textures", new Property("textures", value));
-//        Field profileField = null;
-//        try {
-//            profileField = meta.getClass().getDeclaredField("profile");
-//            profileField.setAccessible(true);
-//            profileField.set(meta, profile);
-//        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-//            e.printStackTrace();
-//        }
-//        head.setItemMeta(meta);
-//        return head;
-//    }
+    public static ItemStack getHeadFromUUID(UUID uuid) {
+        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        GameProfile profile = new GameProfile(UUID.randomUUID(), "");
+        profile.getProperties().put("textures", new Property("textures", headValueCache.get(uuid)));
+        Field profileField = null;
+        try {
+            profileField = meta.getClass().getDeclaredField("profile");
+            profileField.setAccessible(true);
+            profileField.set(meta, profile);
+        } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(meta);
+        return head;
+    }
 
 }
