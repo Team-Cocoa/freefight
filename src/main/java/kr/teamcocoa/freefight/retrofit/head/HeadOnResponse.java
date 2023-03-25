2b6a2bbbc02ce36c00cf5b8276bc3f234807c0fe
fixca <jaehoon1744@gmail.com>
2023-03-25 17:10:07 +0900

    head value 수정 (base64 디코딩 하면 안되더라)

--- src/main/java/kr/teamcocoa/freefight/retrofit/head/HeadOnResponse.java
+++ src/main/java/kr/teamcocoa/freefight/retrofit/head/HeadOnResponse.java
@@ -26,15 +26,9 @@ public class HeadOnResponse implements IResponse<MojangSessionResult> {
             Utils.catchSynchronous();
             Utils.catchAsynchronous();
             JsonObject property = result.getProperties().get(0).getAsJsonObject();
-            String rawValue = property.get("value").getAsString();
-            String decodedValue = new String(Base64.getDecoder().decode(rawValue));
-            JsonObject decodedSkinJson = JSONUtils.parse(decodedValue).getAsJsonObject();
-            String skinValue =
-                    decodedSkinJson.get("textures").getAsJsonObject()
-                    .get("SKIN").getAsJsonObject()
-                    .get("url").getAsString().substring(37);
+            String value = property.get("value").getAsString();
 
-            HeadUtils.getHeadValueCache().put(uuid, skinValue);
+            HeadUtils.getHeadValueCache().put(uuid, value);
             HeadUtils.getNameCache().put(uuid, result.getName());
         }
         else {
