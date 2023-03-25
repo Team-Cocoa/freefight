de1f201c2f42e638834557cca2a215f233b337ec
fixca <jaehoon1744@gmail.com>
2023-03-26 03:37:51 +0900

    인벤토리 클릭시 아이템이 들어오던 버그 수정

--- src/main/java/kr/teamcocoa/freefight/gui/MatchCheckGUI.java
+++ src/main/java/kr/teamcocoa/freefight/gui/MatchCheckGUI.java
@@ -1,12 +1,18 @@
 package kr.teamcocoa.freefight.gui;
 
+import kr.teamcocoa.freefight.kits.Kits;
 import kr.teamcocoa.freefight.main.FreeFight;
+import kr.teamcocoa.freefight.player.FreeFightPlayer;
+import kr.teamcocoa.freefight.player.FreeFightPlayerManager;
 import kr.teamcocoa.freefight.session.result.SessionResult;
 import kr.teamcocoa.freefight.translation.inventories.ResultInventory;
 import kr.teamcocoa.freefight.translation.items.MatchHeadTitle;
 import kr.teamcocoa.freefight.translation.lores.MatchInfoLore;
+import kr.teamcocoa.freefight.translation.messages.KitChangeMessage;
 import kr.teamcocoa.freefight.utils.HeadUtils;
+import kr.teamcocoa.freefight.utils.StringUtils;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -19,6 +25,15 @@ public class MatchCheckGUI extends AbstractGUI {
 
     private static final String INFO_HEAD_VALUE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYWYzM2U3YmIxMjU2YTEyYjVjODhlNzA1ZjIxMjc0ZmQ4NjE4YmJkZTkzYzBkZDNlMjJkOWRiY2YwYjNhMTJiMyJ9fX0=";
 
+    private static MatchCheckGUI instance;
+
+    public static MatchCheckGUI getInstance() {
+        if(instance == null) {
+            instance = new MatchCheckGUI(null);
+        }
+        return instance;
+    }
+
     private SessionResult sessionResult;
 
     public MatchCheckGUI(SessionResult sessionResult) {
@@ -29,7 +44,7 @@ public class MatchCheckGUI extends AbstractGUI {
 
     @Override
     public void openInventory(Player player) {
-        Inventory newInventory = Bukkit.createInventory(null, getSize(), getTitle().getMessage(player));
+        Inventory newInventory = Bukkit.createInventory(null, getSize(), Component.text(getTitle().getMessage(player)));
         newInventory.setContents(getInventory().getContents());
         sessionResult.initInventory(player, newInventory);
 
@@ -50,11 +65,14 @@ public class MatchCheckGUI extends AbstractGUI {
 
     @Override
     public void onClick(InventoryClickEvent e) {
-
+        if(checkThisInventoryClicked(e)) {
+            e.setCancelled(true);
+        }
     }
 
     @Override
     public void onClose(InventoryCloseEvent e) {
 
     }
+
 }
--- src/main/java/kr/teamcocoa/freefight/listener/bukkit/InventoryClickListener.java
+++ src/main/java/kr/teamcocoa/freefight/listener/bukkit/InventoryClickListener.java
@@ -1,6 +1,11 @@
 package kr.teamcocoa.freefight.listener.bukkit;
 
 import kr.teamcocoa.freefight.gui.KitSelectGUI;
+import kr.teamcocoa.freefight.gui.MatchCheckGUI;
+import kr.teamcocoa.freefight.translation.inventories.ResultInventory;
+import kr.teamcocoa.freefight.utils.StringUtils;
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftHumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -10,6 +15,7 @@ public class InventoryClickListener implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent e) {
         KitSelectGUI.getInstance().onClick(e);
+        MatchCheckGUI.getInstance().onClick(e);
     }
 
 }
