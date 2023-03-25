45cd6d4602dd701609e9a1165f16b9d5ae1838f7
fixca <jaehoon1744@gmail.com>
2023-03-25 16:27:51 +0900

    매치 체크 커맨드 구현

--- src/main/java/kr/teamcocoa/freefight/commands/CheckMatchCommand.java
+++ src/main/java/kr/teamcocoa/freefight/commands/CheckMatchCommand.java
@@ -1,13 +1,63 @@
 package kr.teamcocoa.freefight.commands;
 
+import kr.teamcocoa.freefight.gui.MatchCheckGUI;
+import kr.teamcocoa.freefight.main.FreeFight;
+import kr.teamcocoa.freefight.mysql.SessionDatabase;
+import kr.teamcocoa.freefight.session.result.ResultCache;
+import kr.teamcocoa.freefight.session.result.SessionResult;
+import kr.teamcocoa.freefight.utils.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.eclipse.aether.SessionData;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class CheckMatchCommand implements CommandExecutor {
 
+    private static ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 20, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(20));
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+        if(!(commandSender instanceof Player)) {
+            return true;
+        }
+        Player player = ((Player) commandSender);
+
+        if(strings.length < 1) {
+            player.sendMessage(StringUtils.color(FreeFight.getPrefix() + "&cInvalid parameters."));
+            return true;
+        }
+
+        String stringId = strings[0].replaceAll("[^0-9]", "");
+        if(stringId.equalsIgnoreCase("")) {
+            player.sendMessage(StringUtils.color(FreeFight.getPrefix() + "&cInvalid parameters."));
+            return true;
+        }
+
+        int id = Integer.parseInt(stringId);
+
+        executor.execute(() -> {
+            // 캐싱된 result 확인
+            SessionResult result = ResultCache.getResultCache().getOrDefault(id, null);
+            if(result == null) {
+                // db 에서 result 부르기
+                result = SessionDatabase.getResult(id);
+
+                // 그럼에도 불구하고 result 가 null 이라면?
+                // 그냥 존재하지 않는 아이디
+                if(result == null) {
+                    // TODO : 존재 하지 않는 세션 아이디 번역 메시지 추가
+                    player.sendMessage(StringUtils.color(FreeFight.getPrefix() + "&cInvalid parameters."));
+                    return;
+                }
+            }
+            MatchCheckGUI gui = new MatchCheckGUI(result);
+            gui.openInventory(player);
+        });
+        return true;
     }
 }
