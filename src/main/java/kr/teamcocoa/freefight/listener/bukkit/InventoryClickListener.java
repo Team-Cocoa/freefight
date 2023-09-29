9170fe58fef1d642b84265e8c094b69b52d5c21a
fixca <jaehoon1744@gmail.com>
2023-09-29 23:27:15 +0900

    제작 인벤토리 아이템 버그 수정

--- src/main/java/kr/teamcocoa/freefight/listener/bukkit/InventoryClickListener.java
+++ src/main/java/kr/teamcocoa/freefight/listener/bukkit/InventoryClickListener.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 
 public class InventoryClickListener implements Listener {
 
@@ -16,6 +17,11 @@ public class InventoryClickListener implements Listener {
     public void onClick(InventoryClickEvent e) {
         KitSelectGUI.getInstance().onClick(e);
         MatchCheckGUI.getInstance().onClick(e);
+
+        if(e.getClickedInventory().getType() == InventoryType.CRAFTING) {
+            e.setCancelled(true);
+        }
+
     }
 
 }
