9ec1a89b7ce30b91febe4fa630d3fb21bb48e50b
fixca <jaehoon1744@gmail.com>
2023-11-27 01:57:27 +0900

    무승부시 winner loser 가 null 인 경우 NPE 발생 하던 현상 수정

--- src/main/java/kr/teamcocoa/freefight/mysql/SessionDatabase.java
+++ src/main/java/kr/teamcocoa/freefight/mysql/SessionDatabase.java
@@ -98,8 +98,8 @@ public class SessionDatabase {
                                   int player1Hunger, int player2Hunger) {
         MySQL mySQL = connectionPool.getConnection();
         PlaceHolder placeHolder = new PlaceHolder(15);
-        placeHolder.addPlaceHolder(winner.toString());
-        placeHolder.addPlaceHolder(loser.toString());
+        placeHolder.addPlaceHolder(winner != null ? winner.toString() : "");
+        placeHolder.addPlaceHolder(loser != null ? loser.toString() : "");
         placeHolder.addPlaceHolder(player1Inv);
         placeHolder.addPlaceHolder(player1Health);
         placeHolder.addPlaceHolder(player1DamageIn);
