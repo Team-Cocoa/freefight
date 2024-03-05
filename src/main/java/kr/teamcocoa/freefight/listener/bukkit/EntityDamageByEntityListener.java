c6f2ddb64b4fc7c5ff946892da36e34cd6140038
fixca <jaehoon1744@gmail.com>
2024-03-05 21:23:51 +0900

    디버그 로그 제거

--- src/main/java/kr/teamcocoa/freefight/listener/bukkit/EntityDamageByEntityListener.java
+++ src/main/java/kr/teamcocoa/freefight/listener/bukkit/EntityDamageByEntityListener.java
@@ -71,13 +71,13 @@ public class EntityDamageByEntityListener implements Listener {
             }
 
             if(session.getKits() == Kits.LOKA_POT) {
-                Bukkit.getLogger().info(MessageFormat.format(
-                        "Before multi Hitter : {0} Victim : {1} Damage : {2} FinalDamage : {3}",
-                        enemy.getName(), player.getName(), e.getDamage(), e.getFinalDamage()));
+//                Bukkit.getLogger().info(MessageFormat.format(
+//                        "Before multi Hitter : {0} Victim : {1} Damage : {2} FinalDamage : {3}",
+//                        enemy.getName(), player.getName(), e.getDamage(), e.getFinalDamage()));
                 e.setDamage(e.getDamage() * 1.333);
-                Bukkit.getLogger().info(MessageFormat.format(
-                        "After multi Hitter : {0} Victim : {1} Damage : {2} FinalDamage : {3}",
-                        enemy.getName(), player.getName(), e.getDamage(), e.getFinalDamage()));
+//                Bukkit.getLogger().info(MessageFormat.format(
+//                        "After multi Hitter : {0} Victim : {1} Damage : {2} FinalDamage : {3}",
+//                        enemy.getName(), player.getName(), e.getDamage(), e.getFinalDamage()));
             }
 
             sessionEnemy.addDamageOut(e.getFinalDamage());
