b8298696419a4dbc3b69285bda264a99223a511c
fixca <jaehoon1744@gmail.com>
2024-03-06 17:25:24 +0900

    설정 클래스 추가

---
+++ src/main/java/kr/teamcocoa/freefight/settings/FreeFightSetting.java
@@ -0,0 +1,12 @@
+package kr.teamcocoa.freefight.settings;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FreeFightSetting {
+
+    private boolean hideArmor = false;
+
+}
