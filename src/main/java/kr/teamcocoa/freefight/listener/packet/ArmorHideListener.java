e49fce3b8fd4d4801da2a546a24d74b48e0b3ba2
fixca <jaehoon1744@gmail.com>
2024-03-06 11:38:00 +0900

    갑옷 가리는 패킷리스너 추가 (아직 binding 안함)

---
+++ src/main/java/kr/teamcocoa/freefight/listener/packet/ArmorHideListener.java
@@ -0,0 +1,43 @@
+package kr.teamcocoa.freefight.listener.packet;
+
+import com.github.retrooper.packetevents.event.PacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.item.ItemStack;
+import com.github.retrooper.packetevents.protocol.item.type.ItemTypes;
+import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.protocol.player.Equipment;
+import com.github.retrooper.packetevents.protocol.player.EquipmentSlot;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityEquipment;
+import org.bukkit.entity.Player;
+
+public class ArmorHideListener extends PacketListenerAbstract {
+
+    public ArmorHideListener() {
+        super(PacketListenerPriority.HIGHEST);
+    }
+
+    private static ItemStack airItem = new ItemStack.Builder().type(ItemTypes.AIR).build();
+
+    @Override
+    public void onPacketSend(PacketSendEvent e) {
+
+        Player player = (Player) e.getPlayer();
+
+        if(e.getPacketType() != PacketType.Play.Server.ENTITY_EQUIPMENT) {
+            return;
+        }
+
+        WrapperPlayServerEntityEquipment wrappedPacket = new WrapperPlayServerEntityEquipment(e);
+
+        for (Equipment equipment : wrappedPacket.getEquipment()) {
+            if(equipment.getSlot() == EquipmentSlot.HELMET ||
+                    equipment.getSlot() == EquipmentSlot.CHEST_PLATE ||
+                    equipment.getSlot() == EquipmentSlot.LEGGINGS ||
+                    equipment.getSlot() == EquipmentSlot.BOOTS) {
+                equipment.setItem(airItem);
+            }
+        }
+
+    }
+}
