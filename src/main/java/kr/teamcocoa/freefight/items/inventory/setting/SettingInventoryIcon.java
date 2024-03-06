944445e5955f0e9e25a6d50602d039ec6dbb36c8
fixca <jaehoon1744@gmail.com>
2024-03-06 13:17:27 +0900

    세팅 인벤토리에 들어갈 아이템 추상클래스

---
+++ src/main/java/kr/teamcocoa/freefight/items/inventory/setting/SettingInventoryIcon.java
@@ -0,0 +1,22 @@
+package kr.teamcocoa.freefight.items.inventory.setting;
+
+import kr.teamcocoa.freefight.items.AbstractItem;
+import kr.teamcocoa.freefight.settings.FreeFightSetting;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class SettingInventoryIcon extends AbstractItem {
+
+    protected SettingInventoryIcon(Material material) {
+        super(material);
+    }
+
+    public abstract ItemStack toItemStack(Player player, FreeFightSetting setting);
+
+    @Override
+    public ItemStack toItemStack(Player player) {
+        return null;
+    }
+
+}
