7a4632179c3457f41e06d6b2929bc28901ef021a
fixca <jaehoon1744@gmail.com>
2024-03-13 13:32:52 +0900

    builder 패턴 추가

--- src/main/java/kr/teamcocoa/freefight/settings/FreeFightSetting.java
+++ src/main/java/kr/teamcocoa/freefight/settings/FreeFightSetting.java
@@ -1,10 +1,12 @@
 package kr.teamcocoa.freefight.settings;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class FreeFightSetting {
 
     private boolean hideArmor = false;
